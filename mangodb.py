34c9ba206aa76969850fdda0a9604cfa28e0a165
Jonathan-dot150 <joo150@hotmail.com>
2025-09-30 14:19:59 -0400

    Update mangodb.py test

--- mangodb.py
+++ mangodb.py
@@ -9,11 +9,12 @@ from pymongo import MongoClient
 import csv
 
 # connexion Atlas
-client = MongoClient("mongodb+srv://joo150_db_user:Broccoli123@cluster0.nti1pod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MangoSecret
 
 # base + collection
 db = client["bestbuy"]
 collection = db["electronique"]
+
 
 
 
