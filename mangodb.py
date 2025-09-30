54aeea57ed7ea0133809d2a2579ae52ba8ef14b3
Jonathan-dot150 <joo150@hotmail.com>
2025-09-30 14:25:33 -0400

    Update mangodb.py tets connect

--- mangodb.py
+++ mangodb.py
@@ -9,11 +9,12 @@ from pymongo import MongoClient
 import csv
 
 # connexion Atlas
-client = MangoSecret
+client = MongoClient("mongodb+srv://joo150_db_user:Broccoli123@cluster0.nti1pod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # base + collection
 db = client["bestbuy"]
 collection = db["electronique"]
+
 
 
 
