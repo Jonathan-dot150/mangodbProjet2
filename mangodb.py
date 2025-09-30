e4ce83093dd9ef81b3c26c276088116ace95176e
Jonathan-dot150 <joo150@hotmail.com>
2025-09-30 14:14:13 -0400

    Update mangodb.py test connection

--- mangodb.py
+++ mangodb.py
@@ -12,15 +12,9 @@ import csv
 client = MongoClient("mongodb+srv://joo150_db_user:Broccoli123@cluster0.nti1pod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # base + collection
-db = client["ma_base"]
-collection = db["ma_collection"]
+db = client["bestbuy"]
+collection = db["electronique"]
 
-# lire CSV et insérer
-with open("monfichier.csv", newline="", encoding="utf-8") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        collection.insert_one(row)
 
-print("Import terminé ✅")
 
 
